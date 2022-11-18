a9b19a39149890d4ef335c9092960e83caec7606
NogamPixel <67550440+NogamPixel@users.noreply.github.com>
2022-11-18 14:09:30 +0100

    ADD Project Unity enfer édition ADD Post Process Plugin ADD Shader Graph Plugin

---
+++ Unity/TowerDefenseWorkshop_Template/Assets/TD/Scripts/Tower.cs
@@ -0,0 +1,52 @@
+﻿namespace GSGD1
+{
+	using GSGD1;
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	/// <summary>
+	/// Facade for Tower subsystems
+	/// </summary>
+	public class Tower : MonoBehaviour, IPickerGhost, ICellChild
+	{
+		[SerializeField]
+		private WeaponController _weaponController = null;
+
+		[SerializeField]
+		private DamageableDetector _damageableDetector = null;
+
+		private void Awake()
+		{
+			enabled = false;
+		}
+
+		public void Enable(bool isEnabled)
+		{
+			enabled = isEnabled;
+		}
+
+		private void Update()
+		{
+			if (_damageableDetector.HasAnyDamageableInRange() == true)
+			{
+				Damageable damageableTarget = _damageableDetector.GetNearestDamageable();
+				//_weaponController.LookAt(damageableTarget.GetAimPosition());
+				//_weaponController.Fire();
+
+				_weaponController.LookAtAndFire(damageableTarget.GetAimPosition());
+			}
+		}
+
+		// Interfaces
+		public Transform GetTransform()
+		{
+			return transform;
+		}
+
+		public void OnSetChild()
+		{
+			Enable(true);
+		}
+	}
+}
