bc65b28eb198e72bfed693b17e61522450d71a81
NogamPixel <67550440+NogamPixel@users.noreply.github.com>
2022-11-28 10:23:57 +0100

    ADD Spe turret RETAKE Turret stats

--- Unity/TowerDefenseWorkshop_Template/Assets/TD/Scripts/Managers/UIManager.cs
+++ Unity/TowerDefenseWorkshop_Template/Assets/TD/Scripts/Managers/UIManager.cs
@@ -8,11 +8,31 @@ namespace GSGD1
 	public class UIManager : Singleton<UIManager>
 	{
 		[SerializeField]
-		private scr_UITurretMenu _TurretMenu;
+		private scr_UITurretMenu _TurretPlaceMenu;
+		[SerializeField]
+		private scr_TowerUpgradeCanva _TurretUpgradeMenu;
+
+		private bool _aMenuIsOpen = false;
 
 		public scr_UITurretMenu GetUITurretMenu()
         {
-			return _TurretMenu;
+			return _TurretPlaceMenu;
+
+		}
+
+		public scr_TowerUpgradeCanva GetUITowerUpgrade()
+        {
+			return _TurretUpgradeMenu;
+        }
+
+		public bool GetMenuOpen()
+        {
+			return _aMenuIsOpen;
+
+		}
+		public void SetMenuOpen(bool menuOpen)
+		{
+			_aMenuIsOpen = menuOpen;
 
 		}
 	}
